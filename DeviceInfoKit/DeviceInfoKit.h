d45eec24040498a2aa0d87272dd08b7ae615a2fd
Jason Jones <jason.jones3@sky.uk>
2019-09-14 18:04:54 +0100

    Created initial implementation of the app
    Why:
    - This is the first iteration of the app.
    
    This change addresses the need by:
    - Create app icon.
    - Create widget.
    - Create logic to fetch device info.
    - Create DeviceInfoKit.

---
+++ DeviceInfoKit/DeviceInfoKit.h
@@ -0,0 +1,19 @@
+//
+//  DeviceInfoKit.h
+//  DeviceInfoKit
+//
+//  Created by Jones, Jason (Developer) on 14/09/2019.
+//  Copyright © 2019 Jones, Jason (Developer). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceInfoKit.
+FOUNDATION_EXPORT double DeviceInfoKitVersionNumber;
+
+//! Project version string for DeviceInfoKit.
+FOUNDATION_EXPORT const unsigned char DeviceInfoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceInfoKit/PublicHeader.h>
+
+
--- DeviceInfoWidget/TodayViewController.swift
+++ DeviceInfoWidget/TodayViewController.swift
@@ -1,22 +1,29 @@
 //
-//  ViewController.swift
-//  TestInfoHomescreen
+//  TodayViewController.swift
+//  Widget
 //
-//  Created by Jones, Jason (Developer) on 09/09/2019.
+//  Created by Jones, Jason (Developer) on 14/09/2019.
 //  Copyright © 2019 Jones, Jason (Developer). All rights reserved.
 //
 
 import UIKit
 import DeviceInfoKit
+import NotificationCenter
 
-class ViewController: UIViewController {
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
     @IBOutlet weak var iOSVersion: UILabel!
     @IBOutlet weak var deviceModel: UILabel!
     @IBOutlet weak var isProxied: UILabel!
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidLoad() {
+        super.viewDidLoad()
         update()
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        update()
+        completionHandler(NCUpdateResult.newData)
     }
     
     func update() {
@@ -24,5 +31,5 @@ class ViewController: UIViewController {
         deviceModel.text = "Device Model: \(DeviceInfo.deviceModel)"
         isProxied.text   = "Proxy Status: \(DeviceInfo.proxyStatus)"
     }
+    
 }
-
