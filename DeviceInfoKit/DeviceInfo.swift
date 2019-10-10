32c9146f0f30ed2be3828cbb7fda141c95c8e0cc
Jason Jones <jmjones5@gmail.com>
2019-10-10 12:53:52 +0100

    Update device list
    Why:
    - New devices have been release.
    
    This change addresses the need by:
    - Add new iPad and iPhone models.

--- DeviceInfoKit/DeviceInfo.swift
+++ DeviceInfoKit/DeviceInfo.swift
@@ -45,6 +45,7 @@ public struct DeviceInfo {
         switch identifier {
         case "iPod5,1":                                  return "iPod Touch 5"
         case "iPod7,1":                                  return "iPod Touch 6"
+            
         case "iPhone3,1", "iPhone3,2", "iPhone3,3":      return "iPhone 4"
         case "iPhone4,1":                                return "iPhone 4s"
         case "iPhone5,1", "iPhone5,2":                   return "iPhone 5"
@@ -63,11 +64,16 @@ public struct DeviceInfo {
         case "iPhone11,2":                               return "iPhone XS"
         case "iPhone11,4", "iPhone11,6":                 return "iPhone XS Max"
         case "iPhone11,8":                               return "iPhone XR"
+        case "iPhone12,1":                               return "iPhone 11"
+        case "iPhone12,3":                               return "iPhone 11 Pro"
+        case "iPhone12,5":                               return "iPhone 11 Pro Max"
+            
         case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4": return "iPad 2"
         case "iPad3,1", "iPad3,2", "iPad3,3":            return "iPad 3"
         case "iPad3,4", "iPad3,5", "iPad3,6":            return "iPad 4"
         case "iPad4,1", "iPad4,2", "iPad4,3":            return "iPad Air"
         case "iPad5,3", "iPad5,4":                       return "iPad Air 2"
+        case "iPad11,3":                                 return "iPad Air (3rd generation)"
         case "iPad6,11", "iPad6,12":                     return "iPad 5"
         case "iPad7,5", "iPad7,6":                       return "iPad 6"
         case "iPad2,5", "iPad2,6", "iPad2,7":            return "iPad Mini"
@@ -80,9 +86,12 @@ public struct DeviceInfo {
         case "iPad7,3", "iPad7,4":                       return "iPad Pro (10.5-inch)"
         case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4": return "iPad Pro (11-inch)"
         case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8": return "iPad Pro (12.9-inch) (3rd generation)"
+            
         case "AppleTV5,3":                               return "Apple TV"
         case "AppleTV6,2":                               return "Apple TV 4K"
+            
         case "AudioAccessory1,1":                        return "HomePod"
+            
         case "i386", "x86_64":                           return "Simulator \(deviceModel(for: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "iOS"))"
         default:                                         return identifier
         }
